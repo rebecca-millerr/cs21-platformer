9fb02ffa0ccc06d4cd25210e6b7ed1cf301c17da
Luke Taylor <luke@deentaylor.com>
2022-03-30 11:31:56 -0400

    Front-end boilerplate

---
+++ web/next.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src')],
+  },
+
+  webpack: (config, { isServer }) => ({
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        { test: /\.svg$/, use: ['@svgr/webpack'] },
+        { test: /\.glsl$/, use: ['raw-loader', 'glslify-loader'] },
+      ],
+    },
+    plugins: [
+      ...config.plugins,
+      ...(!isServer && process.env.ANALYZE === 'true') ? [
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          reportFilename: './analyze/client.html',
+          generateStatsFile: true,
+        }),
+      ] : [],
+    ],
+  }),
+};
