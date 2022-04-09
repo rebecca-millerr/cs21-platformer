d7b40e90fab30a45e5ed51025d5d349254bc7ade
rebecca-millerr <me@beccamiller.com>
2022-04-08 21:16:24 -0400

    new game page

---
+++ web/src/pages/game.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BLOCKS_ACROSS = 40;
+const BLOCKS_DOWN = 30;
+
+export default function Game() {
+  return <div>This is the game</div>;
+}
+
+// export default Game;
