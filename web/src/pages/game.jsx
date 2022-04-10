c06d0a2df6824f75bf61ec81df20c07cfa68b0da
rebecca-millerr <me@beccamiller.com>
2022-04-10 14:45:04 -0400

    map infinitely adds at the right

--- web/src/pages/game.jsx
+++ web/src/pages/game.jsx
@@ -1,25 +1,28 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-import Map, { getDefaultMap } from 'components/map';
+import { Map, getDefaultMap } from 'components/map';
 
 export default function Game() {
   // TODO: magically gets updates from erlang server and populates
   //       them in state, which updates map
   const [map, setMap] = useState(getDefaultMap());
+  const [timeElapsed, setTimeElapsed] = useState(0);
 
-  // Use useRef for mutable variables that we want to persist
-  // without triggering a re-render on their change
   const requestRef = useRef();
   const previousTimeRef = useRef();
 
   const animate = useCallback((time) => {
     if (previousTimeRef.current !== undefined) {
-      // Pass on a function to the setter of the state
-      // to make sure we always have the latest state
       // TODO: actually replace this state with update from server
-      setMap((prevMap) => prevMap);
+      setMap((prevMap) => [...prevMap]);
+      // setMap((prevMap) => {
+      //   prevMap.shift();
+      //   prevMap.push('000');
+      //   return [...prevMap];
+      // });
     }
     previousTimeRef.current = time;
+    setTimeElapsed(time);
     requestRef.current = requestAnimationFrame(animate);
   }, []);
 
@@ -30,7 +33,7 @@ export default function Game() {
 
   return (
     <div>
-      <Map map={map} />
+      <Map map={map} updateMap={setMap} time={timeElapsed} />
     </div>
   );
 }
