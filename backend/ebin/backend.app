24a80f6a35cb7b04aae5c3a0b4742da193effcff
Tyler Calabrese <tyler.calabrese@pm.me>
2022-04-12 13:15:37 -0400

    Ignore ebin

--- backend/ebin/backend.app
+++ backend/ebin/backend.app
@@ -1,7 +1,7 @@
 {application, 'backend', [
 	{description, "New project"},
 	{vsn, "0.1.0"},
-	{modules, ['backend_app','backend_sup']},
+	{modules, ['backend_app','backend_sup','hello_handler']},
 	{registered, [backend_sup]},
 	{applications, [kernel,stdlib,cowboy]},
 	{mod, {backend_app, []}},
--- backend/src/backend_app.erl
+++ backend/src/backend_app.erl
@@ -5,6 +5,13 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", hello_handler, []}]}
+    ]),
+    {ok, _} = cowboy:start_clear(my_http_listener,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
 	backend_sup:start_link().
 
 stop(_State) ->
