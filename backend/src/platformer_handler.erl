891e242807615ad1a6c52d1cbfbf2af606b2c1d8
Tyler Calabrese <tyler.calabrese@pm.me>
2022-04-16 19:03:56 -0400

    Start handling JSON

--- backend/src/platformer_handler.erl
+++ backend/src/platformer_handler.erl
@@ -6,17 +6,27 @@
 -export([websocket_handle/2]).
 -export([websocket_info/2]).
 
+
 init(Req, State) ->
 	{cowboy_websocket, Req, State}.
 
 websocket_init(State) ->
 	{ok, State}.
 
-websocket_handle({text, Json}, State) ->
-    case (jsx:is_json(Json)) of
-        true ->  Map = jsx:decode(Json),
-                 io:format("received JSON: ~w~n", [Map]),
-	             {reply, {text, Json}, State};
+% For handling decoded JSON data
+handle_json(Json, State) ->
+    case (maps:get(<<"type">>, Json, notype)) of
+        notype -> Res = jsx:encode([{<<"error">>, <<"ill-defined request type">>}]),
+                  {reply, {text, Res}, State};
+        _      -> Res = jsx:encode([{<<"error">>, <<"unrecognized request type">>}]),
+                  {reply, {text, Res}, State}
+    end.
+
+% State is [] on app startup
+websocket_handle({text, Data}, State) ->
+    case (jsx:is_json(Data)) of
+        true ->  Map = jsx:decode(Data),
+                 handle_json(Map, State);
         false -> Res = jsx:encode([{<<"error">>, <<"Message is not valid JSON">>}]),
                  {reply, {text, Res}, State}
     end;
