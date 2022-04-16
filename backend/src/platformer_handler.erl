a0c4d99fbfcceaa82ff2a5d9d493ae9ce7054b11
Tyler Calabrese <tyler.calabrese@pm.me>
2022-04-16 19:21:32 -0400

    Calls and casts

--- backend/src/platformer_handler.erl
+++ backend/src/platformer_handler.erl
@@ -13,20 +13,39 @@ init(Req, State) ->
 websocket_init(State) ->
 	{ok, State}.
 
-% For handling decoded JSON data
-handle_json(Json, State) ->
+% For handling casts based on decoded JSON data. 
+% Sends a response only in the case of an invalid cast.
+% TODO: Replace with console logging and never send a response
+json_cast(Json, State) ->
     case (maps:get(<<"type">>, Json, notype)) of
-        notype -> Res = jsx:encode([{<<"error">>, <<"ill-defined request type">>}]),
+        notype -> Res = jsx:encode([{<<"error">>, <<"Must specify cast type">>}]),
                   {reply, {text, Res}, State};
-        _      -> Res = jsx:encode([{<<"error">>, <<"unrecognized request type">>}]),
+        _      -> Res = jsx:encode([{<<"error">>, <<"unrecognized cast type">>}]),
                   {reply, {text, Res}, State}
     end.
+
+% For handling calls based on decoded JSON data
+json_call(Json, State) ->
+    case (maps:get(<<"type">>, Json, notype)) of
+        notype -> Res = jsx:encode([{<<"error">>, <<"Must specify call type">>}]),
+                  {reply, {text, Res}, State};
+        _      -> Res = jsx:encode([{<<"error">>, <<"unrecognized call type">>}]),
+                  {reply, {text, Res}, State}
+    end.
+
 
 % State is [] on app startup
 websocket_handle({text, Data}, State) ->
     case (jsx:is_json(Data)) of
         true ->  Map = jsx:decode(Data),
-                 handle_json(Map, State);
+                 Call = maps:is_key(<<"call">>, Map),
+                 Cast = maps:is_key(<<"cast">>, Map),
+                 if
+                     Call -> json_call(Map, State);
+                     Cast -> json_cast(Map, State);
+                     true -> Res = jsx:encode([{<<"error">>, <<"Must specify call or cast">>}]),
+                             {reply, {text, Res}, State}
+                 end;
         false -> Res = jsx:encode([{<<"error">>, <<"Message is not valid JSON">>}]),
                  {reply, {text, Res}, State}
     end;
