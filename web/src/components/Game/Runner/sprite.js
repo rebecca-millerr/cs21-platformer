47d4685ba26e4598c1e3fb3597216dc343305f4b
Luke Taylor <luke@deentaylor.com>
2022-04-17 19:03:10 -0400

    Render different sprite states at different speeds

--- web/src/components/Game/Runner/sprite.js
+++ web/src/components/Game/Runner/sprite.js
@@ -12,12 +12,12 @@ const spriteHeight = 37;
 const sheetWidth = 7;
 
 export const sequences = {
-  idle: { seq: [0, 1, 2, 3], loop: true },
-  crouch: { seq: [4, 5, 6, 7], loop: true },
-  run: { seq: [8, 9, 10, 11, 12, 13], loop: true },
-  jump: { seq: [14, 15, 16, 17], loop: false },
-  somersault: { seq: [18, 19, 20, 21], loop: true },
-  fall: { seq: [22, 23], loop: true },
+  idle: { seq: [0, 1, 2, 3], loop: true, pace: 5 },
+  crouch: { seq: [4, 5, 6, 7], loop: true, pace: 10 },
+  run: { seq: [8, 9, 10, 11, 12, 13], loop: true, pace: 10 },
+  jump: { seq: [14, 15, 16, 17], loop: false, pace: 10 },
+  somersault: { seq: [18, 19, 20, 21], loop: true, pace: 10 },
+  fall: { seq: [22, 23], loop: true, pace: 5 },
 };
 
 
@@ -56,7 +56,7 @@ export default class RunnerSprite {
   }
 
   tick() {
-    const frameDelay = 200;
+    const frameDelay = 1000 / sequences[this.state].pace;
     if (Date.now() - this.lastFrameTime > frameDelay) this.nextFrame();
   }
 
