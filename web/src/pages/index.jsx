d7670d018766e3dec847c657b2addfd66abf0782
rebecca-millerr <me@beccamiller.com>
2022-04-24 11:11:46 -0400

    initialize player color

--- web/src/pages/index.jsx
+++ web/src/pages/index.jsx
@@ -10,7 +10,12 @@ const cx = classNames.bind(styles);
 export default function Homepage() {
   const router = useRouter();
   const setPlayerType = useStore((state) => state.setPlayerType);
+  const setPlayerColor = useStore((state) => state.setPlayerColor);
   const join = (desiredPlayerType) => {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    setPlayerColor({ red, green, blue });
     setPlayerType(desiredPlayerType);
     router.push('/game');
   };
@@ -18,17 +23,26 @@ export default function Homepage() {
   return (
     <div className={cx('base')}>
       <h1>Welcome to CS21 Platformer</h1>
-      <button className={cx('join-cta')} type="button" onClick={() => join('runner')}>
+      <button
+        className={cx('join-cta')}
+        type="button"
+        onClick={() => join('runner')}
+      >
         Join as a <strong>runner</strong>
       </button>
-      <button className={cx('join-cta')} type="button" onClick={() => join('builder')}>
+      <button
+        className={cx('join-cta')}
+        type="button"
+        onClick={() => join('builder')}
+      >
         Join as a <strong>builder</strong>
       </button>
 
       <div className={cx('credits')}>
         By <a href="https://github.com/rebecca-millerr">Becca</a>,{' '}
         <a href="https://github.com/tylerjcalabrese">Tyler</a>, and{' '}
-        <a href="https://github.com/controversial">Luke</a> for Tufts CS 21: Concurrent Programming
+        <a href="https://github.com/controversial">Luke</a> for Tufts CS 21:
+        Concurrent Programming
       </div>
     </div>
   );
