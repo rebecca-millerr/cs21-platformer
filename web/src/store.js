f482874a2211f77c399f333005720e49a7f81219
rebecca-millerr <me@beccamiller.com>
2022-04-24 11:09:06 -0400

    store can handle playerColor

--- web/src/store.js
+++ web/src/store.js
@@ -1,10 +1,22 @@
 import create from 'zustand';
 
 const useStore = create((set) => ({
-  playerType: null, // null | runner | builder
+  playerType: null, // null | 'runner' | 'builder'
   setPlayerType: (playerType) => {
-    if (!['runner', 'builder'].includes(playerType)) throw new Error('Invalid player type');
+    if (!['runner', 'builder'].includes(playerType))
+      throw new Error('Invalid player type');
     set(() => ({ playerType }));
+  },
+  playerColor: null, // null | <some string hex color>
+  setPlayerColor: (playerColor) => {
+    if (
+      !playerColor.red ||
+      !playerColor.green ||
+      !playerColor.blue ||
+      Object.values(playerColor).some((val) => val < 0 || val > 255)
+    )
+      throw new Error('Invalid player color');
+    set(() => ({ playerColor }));
   },
 }));
 
