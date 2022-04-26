521bef2aea0c8629061f0eb2deb8d17057eb37f2
Tyler Calabrese <tyler.calabrese@pm.me>
2022-04-25 22:45:09 -0400

    Fix ID generation

--- backend/src/runners_state.erl
+++ backend/src/runners_state.erl
@@ -7,34 +7,35 @@
 
 % State is a map of Pids to runner IDs and a list of recent positions for
 % each runner ID
-server_loop({IDs, Poses}) ->
+server_loop({IDs, Poses, NextID}) ->
     receive
         {add_runner, Pid} ->
-            ID = maps:size(IDs),
-            NewIDs = maps:put(Pid, ID, IDs),
-            Pid ! {id, ID},
+            Pid ! {id, NextID},
             erlang:monitor(process, Pid),
-            server_loop({NewIDs, maps:put(ID, #{x => 0, y => 0}, Poses)});
+            server_loop({
+                maps:put(Pid, NextID, IDs),
+                maps:put(NextID, #{x => 0, y => 0}, Poses),
+                NextID + 1});
         {update, ID, Pos} ->
-            server_loop({IDs, maps:put(ID, Pos, Poses)});
+            server_loop({IDs, maps:put(ID, Pos, Poses), NextID});
         {report, Pid} ->
             Pid ! {IDs, Poses},
-            server_loop({IDs, Poses});
+            server_loop({IDs, Poses, NextID});
         {broadcast} ->
             tick_counter ! {report, self()},
             receive
                 {ticks, Ticks} -> 
                     broadcaster ! {json, #{ticks => Ticks, runners => Poses}},
-                    server_loop({IDs, Poses})
+                    server_loop({IDs, Poses, NextID})
             end;
         {'DOWN', _Ref, process, Pid, Reason} ->
             io:format("runner ~w ended for reason ~w~n", [Pid, Reason]),
             ID = maps:get(Pid, IDs),
-            server_loop({maps:remove(Pid, IDs), maps:remove(ID, Poses)})
+            server_loop({maps:remove(Pid, IDs), maps:remove(ID, Poses), NextID})
     end.
 
 
 start() -> 
     % for now just send every 2 seconds, obv need to make much more often soon
     {ok, _TRef} = timer:send_interval(2000, {broadcast}),
-    server_loop({ #{}, #{} }).
+    server_loop({ #{}, #{}, 0 }).
