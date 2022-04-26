d1a65573a88406bbfa159fa261991fc308ecfb85
Tyler Calabrese <tyler.calabrese@pm.me>
2022-04-25 22:54:59 -0400

    Leave colors for server side

--- backend/src/builders_state.erl
+++ backend/src/builders_state.erl
@@ -4,32 +4,34 @@
 -export([start/0]).
 
 
+del_list(_, []) -> [];
+del_list(X, [X | Xs]) -> Xs;
+del_list(X, [Y | Xs]) -> [Y | del_list(X, Xs)].
 
 % State is a map of Pids to builder IDs
-server_loop({Pids, Colors, NextID}) ->
+server_loop({Pids, IDs, NextID}) ->
     receive
         {add_builder, Pid} ->
             NewPids = maps:put(Pid, NextID, Pids),
-            NewColors = maps:put(NextID, colors:random_rgb(), Colors),
             Pid ! {id, NextID},
             erlang:monitor(process, Pid),
-            server_loop({NewPids, NewColors, NextID + 1});
+            server_loop({NewPids, [NextID | IDs], NextID + 1});
         {report, Pid} ->
-            Pid ! {builders, Pids, Colors},
-            server_loop({Pids, Colors, NextID});
+            Pid ! {builders, Pids, IDs},
+            server_loop({Pids, IDs, NextID});
         {broadcast} ->
             tick_counter ! {report, self()},
             receive
                 {ticks, Ticks} -> 
-                    broadcaster ! {json, #{ticks => Ticks, builders => Colors}},
-                    server_loop({Pids, Colors, NextID})
+                    broadcaster ! {json, #{ticks => Ticks, builders => IDs}},
+                    server_loop({Pids, IDs, NextID})
             end;
         {'DOWN', _Ref, process, Pid, Reason} ->
             io:format("runner ~w ended for reason ~w~n", [Pid, Reason]),
             ID = maps:get(Pid, Pids),
-            server_loop({maps:remove(Pid, Pids), maps:remove(ID, Colors), NextID})
+            server_loop({maps:remove(Pid, Pids), del_list(ID, IDs), NextID})
     end.
 
 
 start() -> 
-    server_loop({#{}, #{}, 0}).
+    server_loop({#{}, [], 0}).
