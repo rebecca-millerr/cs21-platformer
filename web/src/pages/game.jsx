e6fd4dbe593b749ee7aca04b9705d8991f355456
Luke Taylor <luke@deentaylor.com>
2022-04-27 13:32:39 -0400

    Add status bar to game

--- web/src/pages/game.jsx
+++ web/src/pages/game.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import Game, { Ground, ActiveRunner, LevelEditor, PeerBlocks, DebugPane, PeerRunners } from 'components/Game';
+import { useGameContext } from 'components/Game/Game';
 
 import useStore from 'store';
 import { useRouter } from 'next/router';
@@ -8,6 +9,42 @@ import { useRouter } from 'next/router';
 import classNames from 'classnames/bind';
 import styles from './game.module.scss';
 const cx = classNames.bind(styles);
+
+function InstructionsBar() {
+  const { playerType } = useGameContext();
+  if (playerType === 'builder') {
+    return (
+      <div className={cx('instructions')}>
+        <span>Click to place blocks</span>
+      </div>
+    );
+  }
+
+  if (playerType === 'runner') {
+    return (
+      <div className={cx('instructions')}>
+        <div>Use arrow keys to move</div>
+        <div>
+          <kbd><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" /></svg></kbd>
+          <kbd><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg></kbd>
+          <kbd><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clipRule="evenodd" /></svg></kbd>
+        </div>
+      </div>
+    );
+  }
+}
+
+function StatusBar() {
+  const { playerType, ownId, socket } = useGameContext();
+  return (
+    <div className={cx('status')}>
+      <div>{playerType[0].toUpperCase() + playerType.slice(1)} #{ownId}</div>
+      <div>
+        {socket.connected ? 'Connected' : 'Disconnected'}
+      </div>
+    </div>
+  );
+}
 
 export default function GamePage() {
   const playerType = useStore((state) => state.playerType);
@@ -45,37 +82,10 @@ export default function GamePage() {
 
         {/* Debug info */}
         {showDebug && <DebugPane />}
+
+        <StatusBar />
+        <InstructionsBar />
       </Game>
-
-      {playerType === 'builder' && (
-        <div className={cx('instructions')}>
-          <span>Click to place blocks</span>
-        </div>
-      )}
-
-      {playerType === 'runner' && (
-        <div className={cx('instructions')}>
-          <div>Use arrow keys to move</div>
-
-          <div>
-            <kbd>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z" clipRule="evenodd" />
-              </svg>
-            </kbd>
-            <kbd>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-              </svg>
-            </kbd>
-            <kbd>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M5.293 9.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 7.414V15a1 1 0 11-2 0V7.414L6.707 9.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
-              </svg>
-            </kbd>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
