b5fc96df66d60bcd5c0150ac7db8651667e3c391
Luke Taylor <luke@deentaylor.com>
2022-04-29 23:43:43 -0400

    change websocket URL back to localhost

--- web/src/components/Game/constants.js
+++ web/src/components/Game/constants.js
@@ -1,4 +1,4 @@
-export const WEBSOCKET_URL = 'wss://cs21-platformer-backend.ngrok.io';
+export const WEBSOCKET_URL = 'ws://localhost:8080';
 
 // screen is BLOCKS_ACROSS wide, so BLOCKS_ACROSS + 1 can be shown at once
 export const BLOCKS_ACROSS = 16;
